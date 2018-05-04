10c2ae0510b23f2f4968557b4120f76516fa921d
JiangLxx <1063484901@qq.com>
2018-05-04 09:26:26 +0800

    测试发送微信发送消息

---
+++ pagedemo/src/main/java/com/demo/controller/TestWxServlet.java
@@ -0,0 +1,146 @@
+package com.demo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
+
+import com.demo.pojo.WxSignature;
+import com.demo.pojo.wx.resp.TextRespMessage;
+import com.demo.utils.wx.MessageUtil;
+import com.demo.utils.wx.SignUtil2;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestWxServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 7443218762897787265L;
+	
+	/**
+	 * <p>微信签名验证</p>
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		WxSignature sign = new WxSignature();
+        // 随机数
+        sign.setNonce(request.getParameter("nonce"));
+        // 随机字符串
+        sign.setEchostr(request.getParameter("echostr"));
+        // 微信加密签名
+     	sign.setSignature(request.getParameter("signature"));
+        // 时间戳
+        sign.setTimestamp(request.getParameter("timestamp"));
+
+        PrintWriter out = response.getWriter();
+        
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil2.checkSignature(sign)) {
+            out.print(sign.getEchostr());
+        }
+        out.close();  out = null;
+	}
+	
+	/**
+	 * <p>微信消息处理</p>
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 消息的接收、处理、响应
+        // 将请求、响应的编码均设置为UTF-8（防止中文乱码）
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        // 调用核心业务类接收消息、处理消息
+        String respXml = processRequest(request);
+
+        // 响应消息
+        PrintWriter out = response.getWriter();
+        out.print(respXml);
+        out.close();
+	}
+	
+	public String processRequest(HttpServletRequest request) {
+		// xml格式的消息数据
+        String respXml = null;
+        // 默认返回的文本消息内容
+        String respContent = "未知的消息类型！";
+        try {
+        	// 调用parseXml方法解析请求消息
+            Map<String, String> requestMap = MessageUtil.parseXml(request);
+            // 发送方帐号
+            String fromUserName = requestMap.get("FromUserName");
+            // 开发者微信号
+            String toUserName = requestMap.get("ToUserName");
+            // 消息类型
+            String msgType = requestMap.get("MsgType");
+
+            // 回复文本消息
+            TextRespMessage textMessage = new TextRespMessage();
+            textMessage.setToUserName(fromUserName);
+            textMessage.setFromUserName(toUserName);
+            textMessage.setCreateTime(new Date().getTime());
+            textMessage.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_TEXT);
+            
+            // 文本消息
+            if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) {
+                respContent = "您发送的是文本消息！";
+            }
+            // 图片消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) {
+                respContent = "您发送的是图片消息！";
+            }
+            // 语音消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) {
+                respContent = "您发送的是语音消息！";
+            }
+            // 视频消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_VIDEO)) {
+                respContent = "您发送的是视频消息！";
+            }
+            // 视频消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_SHORTVIDEO)) {
+                respContent = "您发送的是小视频消息！";
+            }
+            // 地理位置消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LOCATION)) {
+                respContent = "您发送的是地理位置消息！";
+            }
+            // 链接消息
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) {
+                respContent = "您发送的是链接消息！";
+            }
+            // 事件推送
+            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {
+                // 事件类型
+                String eventType = requestMap.get("Event");
+                // 关注
+                if (eventType.equals(MessageUtil.EVENT_TYPE_SUBSCRIBE)) {
+                    respContent = "谢谢您的关注！";
+                }
+                // 取消关注
+                else if (eventType.equals(MessageUtil.EVENT_TYPE_UNSUBSCRIBE)) {
+                    // TODO 取消订阅后用户不会再收到公众账号发送的消息，因此不需要回复
+                }
+                // 扫描带参数二维码
+                else if (eventType.equals(MessageUtil.EVENT_TYPE_SCAN)) {
+                    // TODO 处理扫描带参数二维码事件
+                }
+                // 上报地理位置
+                else if (eventType.equals(MessageUtil.EVENT_TYPE_LOCATION)) {
+                    // TODO 处理上报地理位置事件
+                }
+                // 自定义菜单
+                else if (eventType.equals(MessageUtil.EVENT_TYPE_CLICK)) {
+                    // TODO 处理菜单点击事件
+                }
+            }
+            // 设置文本消息的内容
+            textMessage.setContent(respContent);
+            // 将文本消息对象转换成xml
+            respXml = MessageUtil.messageToXml(textMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return respXml;
+	}
+}
