147808cadaf6b6630e9470da260d8458c0166c8a
JiangLxx <1063484901@qq.com>
2018-05-21 15:57:19 +0800

    解决ajax跨域问题

---
+++ pagedemo/src/main/java/com/frame/filter/SimpleCorsFilter.java
@@ -0,0 +1,87 @@
+package com.frame.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import com.frame.filter.base.BaseFilter;
+import com.idiot.utils.base.StringHelper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>解决AJAX跨域过滤器</p>
+ * @author JL @date 2018-05-21<br>
+ * @version 1.0<br>
+ */
+public class SimpleCorsFilter extends BaseFilter {
+	/** 起源类型 **/
+	private String access_control_allow_origin = null;
+	/** 报头内容 **/
+	private String access_control_allow_headers = null;
+	/** 请求方式 **/
+	private String access_control_allow_methods = null;
+	/** 授信标识 **/
+	private String access_control_allow_credentials = null;
+	
+	/**
+	 * <p>初始化运行环境监听函数</p>
+	 */
+	public void init(FilterConfig config) throws ServletException {
+		super.init(config);
+		// 起源类型
+		if(StringHelper.isEmpty(access_control_allow_origin)){
+			access_control_allow_origin = getInitParameter("access_control_allow_origin");
+		}
+		// 报头内容
+		if (StringHelper.isEmpty(access_control_allow_headers)) {
+			access_control_allow_headers = getInitParameter("access_control_allow_headers");
+		}
+		// 请求方式
+		if (StringHelper.isEmpty(access_control_allow_methods)) {
+			access_control_allow_methods = getInitParameter("access_control_allow_methods");
+		}
+		// 授信标识
+		if (StringHelper.isEmpty(access_control_allow_credentials)) {
+			access_control_allow_credentials = getInitParameter("access_control_allow_credentials");
+		}
+	}
+	
+	/**
+	 * <p>执行自定义过滤器</p>
+	 */
+	public void doOwnFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Expose-Headers", "*");
+		// 起源类型
+		if (StringHelper.isNotEmpty(access_control_allow_origin)) {
+			response.setHeader("Access-Control-Allow-Origin", access_control_allow_origin);
+		} else {
+			response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		}
+		// 请求方式
+		if (StringHelper.isNotEmpty(access_control_allow_methods)) {
+			response.setHeader("Access-Control-Allow-Methods", access_control_allow_methods);
+		} else {
+			response.setHeader("Access-Control-Allow-Methods", "POST,GET");
+		}
+		// 报头内容
+		if (StringHelper.isNotEmpty(access_control_allow_headers)) {
+			response.setHeader("Access-Control-Allow-Headers", access_control_allow_headers);
+		} else {
+			response.setHeader("Access-Control-Allow-Headers", "Last-Modified, X-Requested-With, Origin, Content-Type");
+		}
+		// 授信标识
+		if (StringHelper.isNotEmpty(access_control_allow_credentials)) {
+			response.setHeader("Access-Control-Allow-Credentials", access_control_allow_credentials);
+		} else {
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+		}
+		// 试探回应
+		if ("OPTIONS".equals(request.getMethod())) {
+			response.setStatus(200);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+}
---
+++ pagedemo/src/main/java/com/frame/filter/base/BaseFilter.java
@@ -0,0 +1,77 @@
+package com.frame.filter.base;
+
+import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+/**
+ * <p>过滤器基本实现类</p>
+ * @author JL @date 2018-05-21<br>
+ * @version 1.0<br>
+ */
+public abstract class BaseFilter implements Filter {
+	/** 用户环境 **/
+	private FilterConfig config = null;
+	/** 会话对象 **/
+	private HttpSession session = null;
+
+	/**
+	 * <p>销毁过滤器时调用的接口函数</p>
+	 */
+	public void destroy() {
+		config = null;
+	}
+
+	/**
+	 * <p>获取会话对象</p>
+	 * @return 会话对象<br>
+	 */
+	public HttpSession getHttpSession() {
+		return session;
+	}
+
+	/**
+	 * <p>获取过滤器环境对象</p>
+	 * @return 环境对象<br>
+	 */
+	public FilterConfig getFilterConfig() {
+		return config;
+	}
+	
+	/**
+	 * <p>获取配置文件中配置的参数值</p>
+	 * @param name 参数名<br>
+	 * @return 参数值<br>
+	 */
+	public String getInitParameter(String name) {
+		return config.getInitParameter(name);
+	}
+	
+	/**
+	 * <p>初始化运行环境监听函数</p>
+	 */
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
+	}
+	
+	/**
+	 * <p>执行过滤功能实现函数</p>
+	 */
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req; 
+		HttpServletResponse response = (HttpServletResponse) resp;
+		session = request.getSession(); doOwnFilter(request, response, chain);
+		
+	}
+	
+	/**
+	 * <p>自定义过滤功能抽象方法</p>
+	 * @param request 请求对象<br>
+	 * @param response 响应对象<br>
+	 * @param chain 过滤链对象<br>
+	 * @throws IOException 异常<br>
+	 * @throws ServletException 异常<br>
+	 */
+	public abstract void doOwnFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException;
+
+}
